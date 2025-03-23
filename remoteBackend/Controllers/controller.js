44936a909c2d84e1dc3d2cbf1e54d5d949055075
Delia-Gherasim <115105029+Delia-Gherasim@users.noreply.github.com>
2025-03-23 20:05:10 +0200

    another one

---
+++ remoteBackend/Controllers/controller.js
@@ -0,0 +1,3 @@
+exports.getHome = (req, res) => {
+  res.json({ message: "Welcome to Node.js Boilerplate API!" });
+};
---
+++ remoteBackend/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+const homeRoutes = require("./Routes/homeRoutes");
+app.use("/", homeRoutes);
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
